feca7948defa6cb5d125ef2220210bae9db0128e
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:34:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
-printDiv("printMe");
 
-function printDiv(id){
-        var printContents = document.getElementById(id).innerHTML;
-        var originalContents = document.body.innerHTML;
-        document.body.innerHTML = printContents;
-        window.print();
-        document.body.innerHTML = originalContents;
-}
+function codespeedy(){
+var print_div = document.getElementById("printMe");
+var print_area = window.open();
+print_area.document.write(print_div.innerHTML);
+print_area.document.close();
+print_area.focus();
+print_area.print();
+print_area.close();
+		}
