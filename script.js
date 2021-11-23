79f2ceaf067950e0033750b5cabe97fc863952c4
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:57:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
-
-var node = document.getElementById('my-node');
-var btn = document.getElementById('foo');
-btn.onclick = function() {
-node.innerHTML = "I'm an image now."
-  domtoimage.toBlob(document.getElementById('my-node'))
-    .then(function(blob) {
-      window.saveAs(blob, 'my-node.png');
-    });
-}
-		
+	
