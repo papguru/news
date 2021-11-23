650ed22b8a49cc101ca1ff9d24d6901e58663ca9
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:07:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
-var gAutoPrint=!0;function processPrint(){if(null!=document.getElementById){var n="<HTML>\n<HEAD>\n";if(null!=document.getElementsByTagName){var e=document.getElementsByTagName("head");e.length>0&&(n+=e[0].innerHTML)}n+="\n</HEAD>\n<BODY>\n";var t=document.getElementById("printMe");if(null==t)return void alert("Error, no contents.");n+=t.innerHTML,n+="\n</BODY>\n</HTML>";var r=window.open("","processPrint");r.document.open(),r.document.write(n),r.document.close(),gAutoPrint&&r.print()}else alert("Browser not supported.")}
+function printDiv(lyrics) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+
+     document.body.innerHTML = printContents;
+
+     window.print();
+
+     document.body.innerHTML = originalContents;
+}
