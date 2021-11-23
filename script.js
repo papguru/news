365030025dffe62f672426fd4a8dcfb668b718a3
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:40:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
 
-function codespeedy(){
-var print_div = document.getElementById("printMe");
-var print_area = window.open();
-print_area.document.write(print_div.innerHTML);
-print_area.document.close();
-print_area.focus();
-print_area.print();
-print_area.close();
-		}
+function printDiv(divName){
+			var printContents = document.getElementById(divName).innerHTML;
+			var originalContents = document.body.innerHTML;
+
+			document.body.innerHTML = printContents;
+
+			window.print();
+
+			document.body.innerHTML = originalContents;
+
+		
