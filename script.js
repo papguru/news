081e38cb77ee270de745f84e19acdb114dfedda7
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:52:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
 
-function printDiv() 
-{
-
-  var divToPrint=document.getElementById('DivIdToPrint');
-
-  var newWin=window.open('','Print-Window');
-
-  newWin.document.open();
-
-  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
-
-  newWin.document.close();
-
-  setTimeout(function(){newWin.close();},10);
-
+var node = document.getElementById('my-node');
+var btn = document.getElementById('foo');
+btn.onclick = function() {
+node.innerHTML = "I'm an image now."
+  domtoimage.toBlob(document.getElementById('my-node'))
+    .then(function(blob) {
+      window.saveAs(blob, 'my-node.png');
+    });
 }
 		
