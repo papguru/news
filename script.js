910c5f950b139f17b0f4469c0abbff907e46d65b
papguru <94305074+papguru@users.noreply.github.com>
2021-11-23 08:29:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function addLink(){for(var e=window.getSelection(),n=document.createElement("div"),o=0;o<e.rangeCount;++o)n.appendChild(e.getRangeAt(o).cloneContents());var t=n.innerHTML+"<br/><br/>Read more:   <br/>"+window.location.href,d=document.createElement("div");d.style.position="absolute",d.style.left="-99999px",document.body.appendChild(d),d.innerHTML=t,e.selectAllChildren(d),window.setTimeout(function(){document.body.removeChild(d)},0)}document.oncopy=addLink;
-function printDiv(printme) {
-       var printContents = document.getElementById(printme).innerHTML;
-       var originalContents = document.body.innerHTML;
-       document.body.innerHTML = "<html><head><title></title></head><body>" + printContents + "</body>";
-       window.print();
-       document.body.innerHTML = originalContents;
-   }
+printDiv("printMe");
+
+function printDiv(id){
+        var printContents = document.getElementById(id).innerHTML;
+        var originalContents = document.body.innerHTML;
+        document.body.innerHTML = printContents;
+        window.print();
+        document.body.innerHTML = originalContents;
+}
